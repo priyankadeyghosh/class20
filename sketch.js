a2bab86b2f1a5f3934ec9c3184a1ae6387bc66fa
priyankadeyghosh <74904505+priyankadeyghosh@users.noreply.github.com>
2021-03-15 16:28:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 var f,m;
 function setup() {
   createCanvas(1200,800);
-  f=createSprite(200,200,50,80);
-  f.shapeColor="blue";
+  f=createSprite(200, 200, 50, 80);
+  f.shapeColor="green";
   m=createSprite(400,200,80,30);
-  m.shapeColor="blue";
+  m.shapeColor="green";
 }
 
 function draw() {
-  background(0,0,0);  
+  background(0);  
   m.x=World.mouseX;
   m.y=World.mouseY;
-  if(m.x-f.x < m.width/2 + f.width/2){
-    f.shapeColor="red";
+  if(m.x-f.x < f.width/2 + m.width/2){
     m.shapeColor="red";
+    f.shapeColor="red";
+  }
+  else {
+    m.shapeColor="green";
+    f.shapeColor="green";
   }
   drawSprites();
 }
